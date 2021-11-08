a5e58325823e629eb823b5161322cf22f5484a00
nadeysha <nadeysha@mail.ru>
2021-11-08 20:06:38 +0400

    fix: fix 06 solution

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,8 +5,10 @@ const srcStyle = path.join(__dirname, "styles");
 const streamStyle = fs.createWriteStream(destStyle);
 const destAsset = path.join(__dirname, "project-dist", "assets");
 const srcAsset = path.join(__dirname, "assets");
-let arr = [];
+const tempFile = fs.createReadStream(path.join(__dirname, "template.html"),"utf-8");
+const indFile = fs.createWriteStream(path.join(__dirname, "project-dist", "index.html"));
 
+let arr = [];
 
 fs.readdir(srcStyle, { withFileTypes: true }, (err, files) => {
   if (err) {
@@ -27,9 +29,75 @@ fs.readdir(srcStyle, { withFileTypes: true }, (err, files) => {
               streamStyle.write(arr.join(""));
             }
           }
-          /* path.join(__dirname, 'project-dist', 'bundle.css'), */
         );
       }
     }
   });
 });
+
+fs.mkdir(path.join(__dirname, "project-dist"), { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+tempFile.on("data", async (data) => {
+  const htmlResult = await htmlBuild();
+  indFile.write(htmlResult);
+
+  async function htmlBuild() {
+    let html = data.toString();
+    const tags = html.match(/{{(.*)}}/gi);
+
+    for (let item of tags) {
+      const tagName = item.substr(2, item.length - 4);
+
+      const componentHTML = await fs.promises.readFile(
+        path.join(__dirname, "components", `${tagName}.html`)
+      );
+      html = html.replace(item, componentHTML.toString());
+    }
+    
+    return html;
+
+  }
+});
+fs.access(destAsset, function (error) {
+  if (error) {
+    console.log('The file is ready for viewing')
+    copyAssets();
+  } else {
+    changeAndDelete();
+  }
+});
+
+function copyAssets() {
+  fs.promises.mkdir(destAsset);
+  copyFiles(srcAsset, destAsset);
+  
+}
+
+async function changeAndDelete() {
+  await fs.promises.rm(destAsset, { recursive: true });
+  await fs.promises.mkdir(destAsset, { recursive: true });
+  copyFiles(srcAsset, destAsset);
+}
+function copyFiles(src, dest) {
+  fs.readdir(src, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach((file) => {
+        const name = file.name.toString();
+        const pathOfSrc = path.join(src, name);
+        const pathOfDest = path.join(dest, name);
+        if (file.isDirectory()) {
+          fs.promises.mkdir(pathOfDest, { recursive: true });
+          copyFiles(pathOfSrc, pathOfDest);
+        } else if (file.isFile()) {
+          fs.promises.copyFile(pathOfSrc, pathOfDest).catch(function (error) {
+            console.log(error);
+          });
+        }
+      });
+    }
+  });
+}
