badd76d40f51a8a399c2343028221139dca48b2d
nadeysha <nadeysha@mail.ru>
2021-11-10 16:37:08 +0400

    fix: fix 02 solution

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,15 +4,20 @@ const path = require("path");
 const pathOfFile = path.join(__dirname, "text.txt");
 const output = fs.createWriteStream(pathOfFile, "utf-8");
 
-console.log('\nHello, enter your text here\n');
-
-process.stdin.on('data', (data) => {
-  if (data.toString().trim() == 'exit') {
+console.log("\nHello, enter your text here\n");
+function close() {
+  process.stdout.write("Удачи в изучении Node.js!");
+  process.exit();
+}
+process.stdin.on("data", (data) => {
+  if (data !== "exit") {
+  }
+  if (data.toString().trim() == "exit") {
+    process.stdout.write("Удачи в изучении Node.js!");
     process.exit();
   }
   output.write(data);
   console.log("\nTo exit enter 'exit' or press Ctrl+c\n");
 });
 
-process.on('exit', () => process.stdout.write("Удачи в изучении Node.js!"));
-
+process.on("SIGINT", close);
